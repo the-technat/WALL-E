39eb0ccc7a498249b1d564f71c48dbc3defd34ca
technat <technat@technat.ch>
2022-04-10 12:35:07 +0200

    feat: add cliphist

---
+++ sway/.config/sway/cliphist.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+## =============================================================================
+##
+## Part of Sway configuration (https://code.immerda.ch/technat/wall-e)
+##
+## Wrapper script for cliphist (Clipboard Manager)
+## doesn't save copied text to history when caller was KeePassXC
+##
+## =============================================================================
+app_id=$( swaymsg -t get_tree | jq -r '.. | select(.type?) | select(.focused==true) | .app_id'  )
+if [[ $app_id != "org.keepassxc.KeePassXC" ]]; then
+  cliphist store
+fi
+
