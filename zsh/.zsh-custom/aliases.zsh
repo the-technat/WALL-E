a14c4b0ea106a6694e19e8c32d7eb71bd265c553
Nathanael <technat@technat.ch>
2022-05-29 11:28:17 +0200

    feat: add shell functions for pgp encryption/decryption

--- zsh/.zsh-custom/aliases.zsh
+++ zsh/.zsh-custom/aliases.zsh
@@ -27,3 +27,15 @@ alias ssh="kitty +kitten ssh"
 alias kak="kubectl apply -k"
 alias kaf="kubectl apply -f"
 
+##################
+# PGP 
+##################
+secret () {
+        output=~/"${1}".$(date +%s).enc
+        gpg --encrypt --armor --output ${output} -r 0x0000 -r 0x0001 -r 0x0002 "${1}" && echo "${1} -> ${output}"
+}
+
+reveal () {
+        output=$(echo "${1}" | rev | cut -c16- | rev)
+        gpg --decrypt --output ${output} "${1}" && echo "${1} -> ${output}"
+}
